6a6dda5ecf177f4104df47e4d12ba83755e034fb
kitokitoki <morihenotegami@gmail.com>
2010-05-30 01:42:30 +0900

    kill-buffer できなくするオプションの追加。リファクタリング

--- scratch-log.el
+++ scratch-log.el
@@ -1,60 +1,118 @@
-;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;; scratch-log.el
 
-;; (get-buffer-create "*scratch*")
+;; Copyright (C) 2010 by kmori
+
+;; Author: kmori <morihenotegami@gmail.com>
+;; Prefix: sl-
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Install
+;; Put this file into load-path'ed directory, and byte compile it if
+;; desired.  And put the following expression into your ~/.emacs.
+;;
+;; (require 'scratch-log.el)
+
+;;; Change Log
+
+;; 0.0.1: scratch-log.el 0.0.1 released.
 
 (eval-when-compile
   (require 'cl))
 
-(defvar scratch-log-file "~/.emacs.d/.scratch-log")
-(defvar prev-scratch-string-file "~/.emacs.d/.scratch-log-prev")
-(defvar restore-scratch-p t)
+(defvar sl-scratch-log-file "~/.emacs.d/.scratch-log")
+(defvar sl-prev-scratch-string-file "~/.emacs.d/.scratch-log-prev")
+(defvar sl-restore-scratch-p t)
+(defvar sl-prohibit-kill-scratch-buffer-p t)
 
-(defun dump-scratch-when-kill-buf ()
+(defun sl-dump-scratch-when-kill-buf ()
   (interactive)
   (when (string= "*scratch*" (buffer-name))
-    (make-prev-scratch-string-file)
-    (append-scratch-log-file)))
+    (sl-make-prev-scratch-string-file)
+    (sl-append-scratch-log-file)))
 
-(defun dump-scratch-when-kill-emacs ()
+(defun sl-dump-scratch-when-kill-emacs ()
   (interactive)
-  (awhen (get-buffer "*scratch*")  
+  (sl-awhen (get-buffer "*scratch*")  
     (with-current-buffer it
-      (make-prev-scratch-string-file)
-      (append-scratch-log-file))))
+      (sl-make-prev-scratch-string-file)
+      (sl-append-scratch-log-file))))
 
-(defun make-prev-scratch-string-file ()
-  (write-region (point-min) (point-max) prev-scratch-string-file))
+(defun sl-make-prev-scratch-string-file ()
+  (write-region (point-min) (point-max) sl-prev-scratch-string-file))
 
-(defun append-scratch-log-file ()
+(defun sl-append-scratch-log-file ()
   (let* ((time (format-time-string "** %Y/%m/%d-%H:%m" (current-time)))
          (buf-str (buffer-substring-no-properties (point-min) (point-max)))
          (contents (concat "\n" time "\n" buf-str)))
     (with-current-buffer (get-buffer-create "tmp")
       (erase-buffer)
       (insert contents)
-      (append-to-file (point-min) (point-max) scratch-log-file))))
+      (append-to-file (point-min) (point-max) sl-scratch-log-file))))
 
-(defun restore-scratch ()
+(defun sl-restore-scratch ()
   (interactive)
-  (when restore-scratch-p
+  (when sl-restore-scratch-p
     (with-current-buffer "*scratch*"
       (erase-buffer)
-      (when (file-exists-p prev-scratch-string-file)
-        (insert-file-contents prev-scratch-string-file)))))
+      (when (file-exists-p sl-prev-scratch-string-file)
+        (insert-file-contents sl-prev-scratch-string-file)))))
+
+(defun sl-scratch-buffer-p ()
+  (if (string= "*scratch*" (buffer-name)) nil t))
 
 ;; Utility
-(defmacro aif (test-form then-form &rest else-forms)
+(defmacro sl-aif (test-form then-form &rest else-forms)
   (declare (indent 2))
   `(let ((it ,test-form))
      (if it ,then-form ,@else-forms)))
 
-(defmacro* awhen (test-form &body body)
+(defmacro* sl-awhen (test-form &body body)
   (declare (indent 1))
-  `(aif ,test-form
+  `(sl-aif ,test-form
        (progn ,@body)))
 
-(add-hook 'kill-buffer-hook 'dump-scratch-when-kill-buf)
-(add-hook 'kill-emacs-hook 'dump-scratch-when-kill-emacs)
-(add-hook 'emacs-startup-hook 'restore-scratch)
+(add-hook 'kill-buffer-hook 'sl-dump-scratch-when-kill-buf)
+(add-hook 'kill-emacs-hook 'sl-dump-scratch-when-kill-emacs)
+(add-hook 'emacs-startup-hook 'sl-restore-scratch)
+(when sl-prohibit-kill-scratch-buffer-p
+  (add-hook 'kill-buffer-query-functions 'sl-scratch-buffer-p))
+
+;;;; Bug report
+(defvar scratch-log-maintainer-mail-address
+  (concat "morihen" "otegami@gm" "ail.com"))
+(defvar scratch-log-bug-report-salutation
+  "Describe bug below, using a precise recipe.
+
+When I executed M-x ...
+
+How to send a bug report:
+  1) Be sure to use the LATEST version of scratch-log.el.
+  2) Enable debugger. M-x toggle-debug-on-error or (setq debug-on-error t)
+  3) Use Lisp version instead of compiled one: (load \"scratch-log.el\")
+  4) If you got an error, please paste *Backtrace* buffer.
+  5) Type C-c C-c to send.
+# If you are a Japanese, please write in Japanese:-)")
+(defun scratch-log-send-bug-report ()
+  (interactive)
+  (reporter-submit-bug-report
+   scratch-log-maintainer-mail-address
+   "scratch-log.el"
+   (apropos-internal "^eldoc-" 'boundp)
+   nil nil
+   scratch-log-bug-report-salutation))
 
 (provide 'scratch-log.el)
